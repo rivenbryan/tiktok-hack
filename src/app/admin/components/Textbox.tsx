5122b9ce7695274d967f3bd94c970ac746fe2073
Bryan Tay <rivenbryan@gmail.com>
2023-09-06 01:09:36 +0800

    finished sales information

--- src/app/admin/components/Textbox.tsx
+++ src/app/admin/components/Textbox.tsx
@@ -4,13 +4,14 @@ type Props = {
   placeholder?: string,
   height?: string;
   handleUpdate?: any;
+  value?: number;
 }
 
-export default function Textbox({placeholder, height, handleUpdate}: Props) {
+export default function Textbox({placeholder, height, handleUpdate, value}: Props) {
   if (height === undefined){
     height = "2.1rem"
   }
   return (
-    <input type="text" onChange={handleUpdate} id="first_name" style={{height: height}}className="border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 " placeholder={placeholder} required/>
+    <input type="text" onChange={handleUpdate} id="first_name" value={value} style={{height: height}}className="border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 " placeholder={placeholder} required/>
   )
 }
--- src/app/admin/components/TierDiscountComponent.tsx
+++ src/app/admin/components/TierDiscountComponent.tsx
@@ -1,36 +1,76 @@
-import React from "react";
+import { TextField } from "@mui/material";
+import React, { useState } from "react";
+import Textbox from "./Textbox";
+import AdminButton from "./AdminButton";
 
 type Props = {};
 
 export default function TierDiscountComponent({}: Props) {
+  const [amt, setAmt] = useState<number>(10);
+  const [discount, setDiscount] = useState<number>(5);
+  const handleUpdate = (e: any)=> {
+    setAmt(e.target.value)
+  }
+  const [check, setCheck] = useState<boolean>(false)
+
+  const handleDiscount = (e: any)=> {
+    setDiscount(e.target.value)
+  }
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-5">
-      <table className="w-full text-sm text-left text-gray-500">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Minimum Quantity
-            </th>
-            <th scope="col" className="px-6 py-3">
-              % Discount
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="bg-white border-b ">
-            <td className="font-bold  px-6 py-4">10</td>
-            <td className="px-6 py-4">5%</td>
-          </tr>
-          <tr className="bg-white border-b ">
-            <td className="font-bold px-6 py-4">20</td>
-            <td className="px-6 py-4">7%</td>
-          </tr>
-          <tr className="bg-white border-b ">
-            <td className="font-bold px-6 py-4">30</td>
-            <td className="px-6 py-4">10%</td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="flex flex-col gap-5">
+      <div className=" shadow-md sm:rounded-lg mt-5">
+        <table className="w-full text-sm text-left text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Minimum Quantity
+              </th>
+              <th scope="col" className="px-6 py-3">
+                % Discount
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b ">
+              <td className="font-bold  px-6 py-4">
+                <Textbox handleUpdate={handleUpdate} value={amt} height="1.5rem" />
+              </td>
+              <td className="px-6 py-4 w-4">
+                <Textbox handleUpdate={handleDiscount} value={discount} height="1.5rem" />
+              </td>
+            </tr>
+            <tr className="bg-white border-b ">
+              <td className="font-bold px-6 py-4">
+                <Textbox value={20} height="1.5rem" />
+              </td>
+              <td className="px-6 py-4 ">
+                <Textbox value={7} height="1.5rem" />
+              </td>
+            </tr>
+            <tr className="bg-white border-b ">
+              <td className="font-bold px-6 py-4">
+                <Textbox value={30} height="1.5rem" />
+              </td>
+              <td className="px-6 py-4">
+                <Textbox value={10} height="1.5rem" />
+              </td>
+            </tr>
+           {
+            check && ( <tr className="bg-white border-b ">
+            <td className="font-bold px-6 py-4">
+                <Textbox height="1.5rem" />
+              </td>
+              <td className="px-6 py-4">
+                <Textbox height="1.5rem" />
+              </td>
+            </tr>)
+           }
+          </tbody>
+        </table>
+      </div>
+      <div className="flex ">
+        <AdminButton handleClick={()=> setCheck(true)} text="Add" />
+      </div>
     </div>
   );
 }
