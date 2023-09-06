7fbc639d1a05b083f4e29794c78a9356ea534e7e
Bryan Tay <rivenbryan@gmail.com>
2023-09-06 23:43:53 +0800

    final change

--- src/app/groupBuyMainPage/join/components/Groupcard.tsx
+++ src/app/groupBuyMainPage/join/components/Groupcard.tsx
@@ -5,22 +5,24 @@ type Props = {};
 export default function Groupcard({}: Props) {
   return (
     <div className="bg-white border border-gray-300 rounded-lg shadow-lg w-[22rem] p-2">
-      <div className="flex gap-2 ">
-        <CgProfile size={30} />
+      <div className="flex gap-5">
+        <div className="flex flex-col items-center justify-between">
+          <CgProfile size={30} />
+          <p className="text-xs">4.95 / 5</p>
+        </div>
         <div className="flex flex-col">
           <h1 className="text-xs">400m away</h1>
-          <h1 className="text-s font-bold">Bryan Team </h1>
+
+          <h1 className="text-s">Bryan Team </h1>
+          <span className="text-[0.6rem] text-rose-600">@bryanhello</span>
+        </div>
+        <div className="flex ml-auto flex-col justify-between">
+          <h1 className="text-green-400 text-xs"> Close in 7 days</h1>
           <div>
             <span className="text-xs text-rose-600">
               $3.51<a className="text-black">/unit</a>
             </span>
           </div>
-        </div>
-        <div className="flex ml-auto flex-col justify-between">
-          <h1 className="text-green-400 text-xs"> Close in 7 days</h1>
-          <button className="bg-red-500 text-white text-sm px-2 py-1 rounded">
-            Click Me
-          </button>
         </div>
       </div>
     </div>
