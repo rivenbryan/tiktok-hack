2b0ea168459687e46bfc46ef565eba919bfb997a
Bryan Tay <rivenbryan@gmail.com>
2023-09-06 14:19:15 +0800

    added container + footer component

---
+++ src/app/groupBuyMainPage/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TiktokMobileFooter from "../components/TiktokMobileFooter";
+import Container from "../components/Container";
+
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+      <Container>
+        <h1>hello</h1>
+      </Container>
+  );
+}
