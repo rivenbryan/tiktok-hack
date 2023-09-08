13d2c7da8729a9a39b3763937d29601c0d6dc315
Bryan Tay <rivenbryan@gmail.com>
2023-09-08 18:01:10 +0800

    added data for listing

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -87,7 +87,7 @@ export default function Listings() {
           <Link href="/order" className="w-[48%] bg-white border rounded-xl">
             <div className="relative">
               <Image
-                src="/productNew.jpg"
+                src="/product2.jpg"
                 width={1000}
                 height={100}
                 alt="pic"
@@ -101,6 +101,165 @@ export default function Listings() {
                   alt="pic"
                   className="w-4 inline-block rounded-full mr-2 border border-white"
                 />
+                Raymond Tay
+              </div>
+            </div>
+
+            <div className="px-2 py-2">
+              <div className="flex flex-row">
+                <h1>
+                  Polo T-Shirt <span>(COLOR)</span>
+                </h1>
+                <Image
+                  src="/product2.jpg"
+                  width={100}
+                  height={100}
+                  alt="pic"
+                  className="w-10 h-10"
+                />
+              </div>
+              <h3 className="text-red-700 font-bold">
+                S$<span className="text-2xl">49.90</span>
+              </h3>
+              <div className="my-2 bg-red-200 inline-block px-2 rounded text-red-700 font-semibold">
+                S$10 off
+              </div>
+
+              <div className="text-sm opacity-60">
+                <Image
+                  src="/star.png"
+                  width={100}
+                  height={100}
+                  alt="pic"
+                  className="h-3 w-3 inline-block mr-2 mb-1"
+                />
+                4.8 | 5.3k sold
+              </div>
+            </div>
+          </Link>
+          <Link href="/order" className="w-[48%] bg-white border rounded-xl">
+            <div className="relative">
+              <Image
+                src="/product3.jpg"
+                width={1000}
+                height={100}
+                alt="pic"
+                className="w-full h-[15rem]"
+              />
+              <div className="flex-row absolute bottom-0 left-0 mb-3 ml-3 text-white inline-block">
+                <Image
+                  src="/product3.jpg"
+                  width={100}
+                  height={100}
+                  alt="pic"
+                  className="w-4 inline-block rounded-full mr-2 border border-white"
+                />
+                Alicia Low
+              </div>
+            </div>
+
+            <div className="px-2 py-2">
+              <div className="flex flex-row">
+                <h1>
+                  Office Bag <span>(with Sling)</span>
+                </h1>
+                <Image
+                  src="/product3.jpg"
+                  width={100}
+                  height={100}
+                  alt="pic"
+                  className="w-10 h-10"
+                />
+              </div>
+              <h3 className="text-red-700 font-bold">
+                S$<span className="text-2xl">79.90</span>
+              </h3>
+              <div className="my-2 bg-red-200 inline-block px-2 rounded text-red-700 font-semibold">
+                S$8 off
+              </div>
+
+              <div className="text-sm opacity-60">
+                <Image
+                  src="/star.png"
+                  width={100}
+                  height={100}
+                  alt="pic"
+                  className="h-3 w-3 inline-block mr-2 mb-1"
+                />
+                4.8 | 145 sold
+              </div>
+            </div>
+          </Link>
+          <Link href="/order" className="w-[48%] bg-white border rounded-xl">
+            <div className="relative">
+              <Image
+                src="/product6.jpg"
+                width={1000}
+                height={100}
+                alt="pic"
+                className="w-full h-[15rem]"
+              />
+              <div className="flex-row absolute bottom-0 left-0 mb-3 ml-3 text-white inline-block">
+                <Image
+                  src="/product6.jpg"
+                  width={100}
+                  height={100}
+                  alt="pic"
+                  className="w-4 inline-block rounded-full mr-2 border border-white"
+                />
+                JiaoHao
+              </div>
+            </div>
+
+            <div className="px-2 py-2">
+              <div className="flex flex-row">
+                <h1>
+                  Running Shoe SG <span>(ALL SIZE)</span>
+                </h1>
+                <Image
+                  src="/product6.jpg"
+                  width={100}
+                  height={100}
+                  alt="pic"
+                  className="w-10 h-10"
+                />
+              </div>
+              <h3 className="text-red-700 font-bold">
+                S$<span className="text-2xl">39.90</span>
+              </h3>
+              <div className="my-2 bg-red-200 inline-block px-2 rounded text-red-700 font-semibold">
+                S$3 off
+              </div>
+
+              <div className="text-sm opacity-60">
+                <Image
+                  src="/star.png"
+                  width={100}
+                  height={100}
+                  alt="pic"
+                  className="h-3 w-3 inline-block mr-2 mb-1"
+                />
+                4.8 | 15.3k sold
+              </div>
+            </div>
+          </Link>
+          <Link href="/order" className="w-[48%] bg-white border rounded-xl">
+            <div className="relative">
+              <Image
+                src="/product1.jpg"
+                width={1000}
+                height={100}
+                alt="pic"
+                className="w-full h-[15rem]"
+              />
+              <div className="flex-row absolute bottom-0 left-0 mb-3 ml-3 text-white inline-block">
+                <Image
+                  src="/product1.jpg"
+                  width={100}
+                  height={100}
+                  alt="pic"
+                  className="w-4 inline-block rounded-full mr-2 border border-white"
+                />
                 Bryan Tay
               </div>
             </div>
@@ -108,10 +267,10 @@ export default function Listings() {
             <div className="px-2 py-2">
               <div className="flex flex-row">
                 <h1>
-                  Plain T-Shirt <span>(NORMAL)</span>
+                  Fresh T-Shirt <span>(NORMAL)</span>
                 </h1>
                 <Image
-                  src="/productNew.jpg"
+                  src="/product1.jpg"
                   width={100}
                   height={100}
                   alt="pic"
@@ -140,7 +299,7 @@ export default function Listings() {
           <Link href="/order" className="w-[48%] bg-white border rounded-xl">
             <div className="relative">
               <Image
-                src="/productNew.jpg"
+                src="/product4.jpg"
                 width={1000}
                 height={100}
                 alt="pic"
@@ -161,7 +320,7 @@ export default function Listings() {
             <div className="px-2 py-2">
               <div className="flex flex-row">
                 <h1>
-                  Plain T-Shirt <span>(NORMAL)</span>
+                  Flower Bag <span>(NORMAL)</span>
                 </h1>
                 <Image
                   src="/productNew.jpg"
@@ -172,166 +331,7 @@ export default function Listings() {
                 />
               </div>
               <h3 className="text-red-700 font-bold">
-                S$<span className="text-2xl">9.90</span>
-              </h3>
-              <div className="my-2 bg-red-200 inline-block px-2 rounded text-red-700 font-semibold">
-                S$3 off
-              </div>
-
-              <div className="text-sm opacity-60">
-                <Image
-                  src="/star.png"
-                  width={100}
-                  height={100}
-                  alt="pic"
-                  className="h-3 w-3 inline-block mr-2 mb-1"
-                />
-                4.8 | 15.3k sold
-              </div>
-            </div>
-          </Link>
-          <Link href="/order" className="w-[48%] bg-white border rounded-xl">
-            <div className="relative">
-              <Image
-                src="/productNew.jpg"
-                width={1000}
-                height={100}
-                alt="pic"
-                className="w-full h-[15rem]"
-              />
-              <div className="flex-row absolute bottom-0 left-0 mb-3 ml-3 text-white inline-block">
-                <Image
-                  src="/productNew.jpg"
-                  width={100}
-                  height={100}
-                  alt="pic"
-                  className="w-4 inline-block rounded-full mr-2 border border-white"
-                />
-                Bryan Tay
-              </div>
-            </div>
-
-            <div className="px-2 py-2">
-              <div className="flex flex-row">
-                <h1>
-                  Plain T-Shirt <span>(NORMAL)</span>
-                </h1>
-                <Image
-                  src="/productNew.jpg"
-                  width={100}
-                  height={100}
-                  alt="pic"
-                  className="w-10 h-10"
-                />
-              </div>
-              <h3 className="text-red-700 font-bold">
-                S$<span className="text-2xl">9.90</span>
-              </h3>
-              <div className="my-2 bg-red-200 inline-block px-2 rounded text-red-700 font-semibold">
-                S$3 off
-              </div>
-
-              <div className="text-sm opacity-60">
-                <Image
-                  src="/star.png"
-                  width={100}
-                  height={100}
-                  alt="pic"
-                  className="h-3 w-3 inline-block mr-2 mb-1"
-                />
-                4.8 | 15.3k sold
-              </div>
-            </div>
-          </Link>
-          <Link href="/order" className="w-[48%] bg-white border rounded-xl">
-            <div className="relative">
-              <Image
-                src="/productNew.jpg"
-                width={1000}
-                height={100}
-                alt="pic"
-                className="w-full h-[15rem]"
-              />
-              <div className="flex-row absolute bottom-0 left-0 mb-3 ml-3 text-white inline-block">
-                <Image
-                  src="/productNew.jpg"
-                  width={100}
-                  height={100}
-                  alt="pic"
-                  className="w-4 inline-block rounded-full mr-2 border border-white"
-                />
-                Bryan Tay
-              </div>
-            </div>
-
-            <div className="px-2 py-2">
-              <div className="flex flex-row">
-                <h1>
-                  Plain T-Shirt <span>(NORMAL)</span>
-                </h1>
-                <Image
-                  src="/productNew.jpg"
-                  width={100}
-                  height={100}
-                  alt="pic"
-                  className="w-10 h-10"
-                />
-              </div>
-              <h3 className="text-red-700 font-bold">
-                S$<span className="text-2xl">9.90</span>
-              </h3>
-              <div className="my-2 bg-red-200 inline-block px-2 rounded text-red-700 font-semibold">
-                S$3 off
-              </div>
-
-              <div className="text-sm opacity-60">
-                <Image
-                  src="/star.png"
-                  width={100}
-                  height={100}
-                  alt="pic"
-                  className="h-3 w-3 inline-block mr-2 mb-1"
-                />
-                4.8 | 15.3k sold
-              </div>
-            </div>
-          </Link>
-          <Link href="/order" className="w-[48%] bg-white border rounded-xl">
-            <div className="relative">
-              <Image
-                src="/productNew.jpg"
-                width={1000}
-                height={100}
-                alt="pic"
-                className="w-full h-[15rem]"
-              />
-              <div className="flex-row absolute bottom-0 left-0 mb-3 ml-3 text-white inline-block">
-                <Image
-                  src="/productNew.jpg"
-                  width={100}
-                  height={100}
-                  alt="pic"
-                  className="w-4 inline-block rounded-full mr-2 border border-white"
-                />
-                Bryan Tay
-              </div>
-            </div>
-
-            <div className="px-2 py-2">
-              <div className="flex flex-row">
-                <h1>
-                  Plain T-Shirt <span>(NORMAL)</span>
-                </h1>
-                <Image
-                  src="/productNew.jpg"
-                  width={100}
-                  height={100}
-                  alt="pic"
-                  className="w-10 h-10"
-                />
-              </div>
-              <h3 className="text-red-700 font-bold">
-                S$<span className="text-2xl">9.90</span>
+                S$<span className="text-2xl">49.90</span>
               </h3>
               <div className="my-2 bg-red-200 inline-block px-2 rounded text-red-700 font-semibold">
                 S$3 off
