183f1c876ed606d2c3509ed37666f9f2ffeaaef1
Andrew Oak <andrewoaksoekhant@gmail.com>
2023-09-09 13:54:24 +0800

    Roll over toggle

--- src/app/components/TiktokMobileHeader.tsx
+++ src/app/components/TiktokMobileHeader.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 import { useRouter } from "next/navigation";
+import { Switch } from "@/components/ui/switch";
 
 type Props = {
   heading?: string;
@@ -10,9 +11,23 @@ type Props = {
 };
 
 export default function TiktokMobileHeader({ heading, navigateURL }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
   return (
     <>
+      <div
+        className={`gap-3 p-2 flex flex-col items-center fixed top-7 right-2 w-32 bg-rose-600 shadow rounded-lg z-10 text-white font-bold ${
+          isOpen ? "block" : "hidden"
+        }`}
+      >
+        <div className="flex w-full justify-between items-center">
+          <p className="text-sm">Rollover</p>
+          <Switch className="bg-white " defaultChecked />
+        </div>
+        <button className="text-sm bg-white text-rose-600 px-1 rounded-md w-full">
+          Leave group
+        </button>
+      </div>
       <div className="bg-white p-2 flex gap-5 justify-between items-center">
         <div
           onClick={() => {
@@ -26,7 +41,13 @@ export default function TiktokMobileHeader({ heading, navigateURL }: Props) {
           <AiOutlineArrowLeft color="black" size={30} />
         </div>
         {<div className="flex-grow-1 font-bold">{heading}</div>}
-        <BiDotsHorizontalRounded color="black" size={30} />
+        <BiDotsHorizontalRounded
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+          color="black"
+          size={30}
+        />
       </div>
       <hr className="h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
     </>
